5c44ea872b8ef9dd8a48ba435cf986ec483f5277
SayQuit <501241011@qq.com>
2024-01-09 14:01:27 +0800

    忘记密码icon

--- src/page/loginPage/index.jsx
+++ src/page/loginPage/index.jsx
@@ -1,6 +1,6 @@
 import './index.css'
 import { Button, Input, Checkbox } from 'antd';
-import { QuestionOutlined } from '@ant-design/icons';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 
 function LoginPage() {
     return (
@@ -11,7 +11,7 @@ function LoginPage() {
                 <Input placeholder="输入密码" className='mt-2' />
                 <div className="login-action mt-2 w-full">
                     <Checkbox className='login-remember'>记住密码</Checkbox>
-                    <div className='login-forget'>忘记密码<QuestionOutlined /></div>
+                    <div className='login-forget'>忘记密码 <QuestionCircleOutlined /></div>
                 </div>
                 <Button className='mt-2 w-full login-button'>登录</Button>
                 <div className="login-register w-full mt-2">您还没有账号？<span>注册</span></div>
