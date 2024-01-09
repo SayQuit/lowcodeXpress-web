9515c297ac2051b98e8245ad19dafdb446f2ed79
SayQuit <501241011@qq.com>
2024-01-09 15:30:56 +0800

    registerPage

---
+++ src/page/registerPage/index.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import { Button, Input } from 'antd';
+
+function RegisterPage() {
+    return (
+        <div className='register'>
+            <div className='register-block'>
+                <div className="register-title">注册</div>
+                <Input placeholder="输入用户名" className='mt-2' />
+                <Input.Password placeholder="输入密码" className='mt-2' />
+                <Input.Password  placeholder="确认密码" className='mt-2' />
+                <Button className='mt-2 w-full register-button'>注册</Button>
+            </div>
+        </div>
+    );
+}
+
+export default RegisterPage;
