5a1e2f614d4ac89bc2c146bd2549ea09b2bd9aee
SayQuit <501241011@qq.com>
2024-01-17 14:02:12 +0800

    app全局

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css'
-
+import LoginPage from './page/loginPage';
+import MainPage from './page/mainPage'
+import RegisterPage from './page/registerPage';
+import ProjectSetup from './page/projectSetup';
+import PageHeader from './component/pageHeader';
 function App() {
   return (
-    <div>
+    <div className='app'>
+      <div className='app-header'>
+        <PageHeader></PageHeader>
+      </div>
+      <div className='app-main'>
+        <MainPage></MainPage>
+      </div>
     </div>
   );
 }
