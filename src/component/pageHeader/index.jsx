c957de052f2acbeeacf4076071b1c4f394edb26a
SayQuit <501241011@qq.com>
2024-01-17 12:28:03 +0800

    header

---
+++ src/component/pageHeader/index.jsx
@@ -0,0 +1,21 @@
+import './index.css'
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+
+function PageHeader() {
+  return (
+    <div className="pageHeader">
+      <div className='pageHeader-main pageHeader-hover'>首页</div>
+      <div className='float-right'>
+        <div className='pageHeader-button pageHeader-hover'>登录</div>
+        <div className='pageHeader-button pageHeader-hover'>注册</div>
+        <div className='pageHeader-avater'>
+          <Avatar shape="square" icon={<UserOutlined />} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageHeader;
