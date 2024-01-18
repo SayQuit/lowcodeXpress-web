4b72e71bc2c5bdd7c3aebd9cd49ce0d439135df6
SayQuit <501241011@qq.com>
2024-01-18 21:01:00 +0800

    请求体bug修复

--- src/request/index.js
+++ src/request/index.js
@@ -2,98 +2,77 @@ import service from "./service";
 
 export const testRequest = () => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/test',
-            method: 'post',
-            params: {}
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/test', {})
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
 
 export const loginRequest = (account, password) => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/user/login',
-            method: 'post',
-            params: { account, password }
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/user/register', { account, password })
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
 
 export const registerRequest = (username, password) => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/user/register',
-            method: 'post',
-            params: { username, password }
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/user/register', { username, password })
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
 
 export const tokenLoginRequest = () => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/user/tokenLogin',
-            method: 'post',
-            params: {}
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/user/tokenLogin', {})
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
 
 export const createProjectRequest = (json, name, description) => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/project/create',
-            method: 'post',
-            params: { json, name, description }
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/project/create', { json, name, description })
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
 
 export const setProjectRequest = (id, json, name, description) => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/project/set',
-            method: 'post',
-            params: {id, json, name, description}
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/project/set', { id, json, name, description })
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
 
 export const getProjectRequest = () => {
     return new Promise((resolve, reject) => {
-        service({
-            url: '/project/get',
-            method: 'post',
-            params: {}
-        }).then((res) => {
-            resolve(res.data)
-        }).catch((error) => {
-            reject(error)
-        })
+        service.post('/project/get', {})
+            .then((res) => {
+                resolve(res.data)
+            }).catch((error) => {
+                reject(error)
+            })
     })
 }
