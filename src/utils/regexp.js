812d4596045c7efa2b36da5601b8cf6c06e14ce7
SayQuit <501241011@qq.com>
2024-01-18 20:58:36 +0800

    用户名和密码正则判断

---
+++ src/utils/regexp.js
@@ -0,0 +1,11 @@
+export const passwordRegExp = (password) => {
+    const pattern = /^(?=.*[A-Za-z])(?=.*\d)[\S]{8,16}$/;
+    if (pattern.test(password)) return true
+    else return false
+}
+
+export const usernameRegExp = (username) => {
+    const pattern = /^[^\s]{1,10}$/;
+    if (pattern.test(username)) return true
+    else return false
+}
