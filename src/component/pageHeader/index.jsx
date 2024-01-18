aa745f192975925966af60fcbb8a0bc3be5761ae
SayQuit <501241011@qq.com>
2024-01-18 15:37:03 +0800

    部分样式修改

--- src/component/pageHeader/index.jsx
+++ src/component/pageHeader/index.jsx
@@ -7,7 +7,7 @@ function PageHeader() {
   return (
     <div className="pageHeader">
       <div className='pageHeader-main pageHeader-hover'>首页</div>
-      <div className='float-right'>
+      <div className='flex'>
         <div className='pageHeader-button pageHeader-hover'>登录</div>
         <div className='pageHeader-button pageHeader-hover'>注册</div>
         <div className='pageHeader-avater'>
--- src/page/registerPage/index.jsx
+++ src/page/registerPage/index.jsx
@@ -7,9 +7,9 @@ function RegisterPage() {
             <div className='register-block'>
                 <div className="register-title">注册</div>
                 <Input placeholder="输入用户名" className='mt-2' />
-                <Input.Password placeholder="输入密码" className='mt-2' />
-                <Input.Password  placeholder="确认密码" className='mt-2' />
-                <Button className='mt-2 w-full register-button'>注册</Button>
+                <Input.Password placeholder="输入密码" className='mt-2' size='large' />
+                <Input.Password placeholder="确认密码" className='mt-2' size='large' />
+                <Button className='mt-2 w-full register-button' size='large'>注册</Button>
             </div>
         </div>
     );
