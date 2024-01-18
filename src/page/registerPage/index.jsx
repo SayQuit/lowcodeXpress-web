554a1bf4ed25ffed1da41cd48438725aca44fc96
SayQuit <501241011@qq.com>
2024-01-18 17:56:34 +0800

    注册页输入框size

--- src/page/registerPage/index.jsx
+++ src/page/registerPage/index.jsx
@@ -6,7 +6,7 @@ function RegisterPage() {
         <div className='register'>
             <div className='register-block'>
                 <div className="register-title">注册</div>
-                <Input placeholder="输入用户名" className='mt-2' />
+                <Input placeholder="输入用户名" className='mt-2' size='large'/>
                 <Input.Password placeholder="输入密码" className='mt-2' size='large' />
                 <Input.Password placeholder="确认密码" className='mt-2' size='large' />
                 <Button className='mt-2 w-full register-button' size='large'>注册</Button>
