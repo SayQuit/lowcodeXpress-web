51c4144078a9c56108d6bfe30d7ffc8442e45e7e
SayQuit <501241011@qq.com>
2024-01-19 16:00:38 +0800

    初始化阶段

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { HashRouter } from 'react-router-dom';
 import './App.css'
 import PageHeader from './component/pageHeader';
 import RouterPage from './router/RouterPage'
+import { tokenLoginRequest } from './request';
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{
+    tokenLoginRequest()
+  },[])
   return (
     <div className='app'>
       <HashRouter>
--- src/page/mainPage/index.jsx
+++ src/page/mainPage/index.jsx
@@ -1,8 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import './index.css'
 import { Card } from 'antd'
+import { useEffect, useState } from 'react';
+import { getProjectRequest } from '../../request';
 
 function MainPage() {
+  const [projectList, setProjectList] = useState([])
+  useEffect(() => {
+    getProjectList()
+  }, [])
+  const getProjectList = async () => {
+    const { projectList: list } = await getProjectRequest()
+    setProjectList(list.map(item => {
+      const { createAt, id, json } = item
+      return {
+        id,
+        createAt,
+        ...JSON.parse(json)
+        // 这里需要评估下element的问题
+      }
+    }))
+  }
+
   return (
     <div className='mt-2 mb-2'>
       <NavLink to={{ pathname: '/setup' }}>
@@ -19,15 +38,15 @@ function MainPage() {
         </div>
       </NavLink>
       {
-        [1, 2, 3, 4, 5, 1, 2, 3, 4, 1, 2, 3, 4, 5].map((item, index) =>
+        projectList.map((item, index) =>
         (<div className='mainPage-item' key={index}>
           <div className='mainPage-item-block'>
             <Card bordered={true}>
               <div className='mainPage-item-block-img'>
                 <img src="https://gw.alipayobjects.com/zos/bmw-prod/1c363c0b-17c6-4b00-881a-bc774df1ebeb.svg" alt="" />
               </div>
-              <div className='mainPage-item-block-title'>项目标题</div>
-              <div className='mainPage-item-block-desc'>项目描述{item}</div>
+              <div className='mainPage-item-block-title'>{item.name}</div>
+              <div className='mainPage-item-block-desc'>{item.desc}</div>
             </Card>
           </div>
         </div>))
