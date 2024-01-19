0a9faec4c595ac4046a5273dd6b04ca9520e5d64
SayQuit <501241011@qq.com>
2024-01-19 16:00:09 +0800

    setup页面选项

---
+++ src/page/projectSetup/options.js
@@ -0,0 +1,38 @@
+export const options = {
+    projectType: [
+        {
+            value: 'wechat mini program',
+            label: '微信小程序'
+        },
+        {
+            value: 'web',
+            label: 'Web项目'
+        }
+    ],
+    projectTech: [
+        {
+            value: 'normal',
+            label: '原生'
+        }, {
+            value: 'vue',
+            label: 'Vue.js'
+        }, {
+            value: 'react',
+            label: 'React.js'
+        }
+    ],
+    projectLib: [
+        {
+            value: 'element-ui',
+            label: 'Element-UI'
+        }, {
+            value: 'ant-design',
+            label: 'Ant-Design'
+        }, {
+            value: 'echarts',
+            label: 'Echarts'
+        }
+    ]
+
+
+}
