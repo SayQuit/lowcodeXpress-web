3dfb5cc313861cc22ae48d2e60d2eb85ede3ca86
SayQuit <501241011@qq.com>
2024-01-28 19:59:53 +0800

    board页面修改

--- src/page/projectPage/component/projectBoard/component/boardContainer.jsx
+++ src/page/projectPage/component/projectBoard/component/boardContainer.jsx
@@ -2,22 +2,24 @@ import '../../../index.css'
 import { useContext } from 'react';
 import { useDrop } from 'react-dnd';
 import { LeftSiderContext } from '../../../provider/leftSiderProvider';
+import { ElementContext } from '../../../provider/elementProvider';
 
-function BoardConatiner({ handleDrop, children }) {
+function BoardConatiner({ children }) {
+
+    const { onElementSelectVisibleChange } = useContext(LeftSiderContext);
+    const { elementPush } = useContext(ElementContext);
 
     const [{ isOver }, dropRef] = useDrop(() => ({
         accept: 'ELEMENT_ITEM',
-        drop: (item) => handleDrop(item),
+        drop: (item) => elementPush(item),
         collect: (monitor) => ({
             isOver: monitor.isOver(),
         }),
     }));
 
-    const { onElementSelectVisibleChange } = useContext(LeftSiderContext);
-
     return (
-        <div className={`board-container ${isOver ? 'board-container-hover' : ''}`} ref={dropRef} onClick={onElementSelectVisibleChange}>
-            <div className="board-container-tips">点击添加元素</div>
+        <div className={`board-container ${isOver ? 'board-container-hover' : ''}`} ref={dropRef}>
+            {children ? children : <div className="board-container-tips" onClick={onElementSelectVisibleChange}>点击添加元素</div>}
         </div>
     );
 }
--- src/page/projectPage/component/projectBoard/index.jsx
+++ src/page/projectPage/component/projectBoard/index.jsx
@@ -1,10 +1,21 @@
 import '../../index.css'
 import BoardConatiner from './component/boardContainer';
+import { ElementContext } from '../../provider/elementProvider';
+import { useContext } from 'react';
 
 function ProjectBoard() {
+
+    const { component } = useContext(ElementContext)
+
     return (
         <div className="project-main-board">
-            <BoardConatiner></BoardConatiner>
+            {component.map((children,index) => {
+                return (
+                    <BoardConatiner key={index}>
+                        {children}
+                    </BoardConatiner>
+                )
+            })}
         </div>
     );
 }
