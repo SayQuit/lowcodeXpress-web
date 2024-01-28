24de85a67b6ade63aaf770fbd1acfec968cf9476
SayQuit <501241011@qq.com>
2024-01-28 19:59:06 +0800

    provider完善

--- src/page/projectPage/provider/elementProvider.js
+++ src/page/projectPage/provider/elementProvider.js
@@ -2,6 +2,7 @@
 import React, { createContext, useCallback, useEffect, useState } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { getProjectDetailRequest } from '../../../request';
+import { parseObjectToComponent } from '../utils/parser'
 
 export const ElementContext = createContext();
 
@@ -9,8 +10,10 @@ export const ElementProvider = ({ children }) => {
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
 
-    const [detail, setDetail] = useState([])
-    const [activeNode, setActiveNode] = useState(null)
+    const [detail, setDetail] = useState({})
+    const [element, setElement] = useState([])
+    const [component, setComponent] = useState([])
+    // const [activeNode, setActiveNode] = useState(null)
 
     const getProjectDetail = useCallback(async (id) => {
         const res = await getProjectDetailRequest(id)
@@ -19,8 +22,22 @@ export const ElementProvider = ({ children }) => {
             return
         }
         setDetail(res.data)
+        setElement([{ type: 'text', attr: { style: 'font-size:24px;' } }, { type: 'container' },])
     }, [navigate])
 
+    useEffect(() => {
+        console.log(element);
+        setComponent(parseObjectToComponent(element))
+    }, [element])
+
+    const elementPush = (item) => {
+        const { type } = item
+        const newElm = [...element]
+        newElm.splice(element.length-1, 1)
+        newElm.push({ type }, { type: 'container' })
+        console.log(newElm);
+        setElement(newElm)
+    }
 
     useEffect(() => {
         const id = searchParams.get('id')
@@ -29,7 +46,7 @@ export const ElementProvider = ({ children }) => {
     }, [searchParams, navigate, getProjectDetail])
 
     return (
-        <ElementContext.Provider value={{ detail }}>
+        <ElementContext.Provider value={{ detail, elementPush, component }}>
             {children}
         </ElementContext.Provider>
     );
