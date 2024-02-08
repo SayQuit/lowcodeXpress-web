776aa8c5f039b6443000e8bb46ad94183ef20a30
SayQuit <501241011@qq.com>
2024-02-08 18:37:59 +0800

    删除console.log

--- src/utils/style.js
+++ src/utils/style.js
@@ -2,7 +2,6 @@ export const convertToHyphenated = (property) => {
     return property.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 }
 export const removePxFromString = (inputString) => {
-    console.log(inputString);
     const result = inputString.replace(/px/g, '');
     const parsedValue = parseFloat(result);
     return isNaN(parsedValue) ? '' : parsedValue;
