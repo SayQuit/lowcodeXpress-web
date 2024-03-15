018f9b2b39075270042c2f33ac97ee3f49cd5523
SayQuit <501241011@qq.com>
2024-03-15 12:09:47 +0800

    优化右侧栏逻辑

--- src/page/projectPage/component/rightSider/index.jsx
+++ src/page/projectPage/component/rightSider/index.jsx
@@ -1,48 +1,47 @@
-import { useState, useContext } from 'react';
-import '../../style/right.css'
-import StyleSelector from './styleSelector';
-import AttrSelector from './attrSelector'
-import ConfigSelector from './configSelector';
+import React, { useState, useContext, useEffect } from 'react';
 import { Tabs, Empty } from 'antd';
-import { ElementContext } from '../../provider/elementProvider';
+import '../../style/right.css';
+import StyleSelector from './styleSelector';
+import AttrSelector from './attrSelector';
+import ConfigSelector from './configSelector';
 import CircleSelector from './circleSelector';
-
+import { ElementContext } from '../../provider/elementProvider';
+const regularTabs = [
+    { key: 'style', label: '样式' },
+    { key: 'attr', label: '属性' },
+    { key: 'config', label: '配置' },
+];
+const circleTabs = [
+    { key: 'style', label: '样式' },
+    { key: 'circle', label: '循环依赖配置' },
+    { key: 'config', label: '配置' },
+];
 function RightSider() {
-    const { isElementActive, activeElement, activeElementID } = useContext(ElementContext)
-    const [activeKey, setActiveKey] = useState('style')
-    const items = [
-        {
-            key: 'style',
-            label: '样式',
-        },
-        {
-            key: 'config',
-            label: '配置',
-        },
-        {
-            key: 'attr',
-            label: '属性',
-        },
-    ];
+    const { isElementActive, activeElement } = useContext(ElementContext);
+    const [activeKey, setActiveKey] = useState('style');
+    const [tabsItems, setTabsItems] = useState([]);
+
+    useEffect(() => {
+        if (isElementActive && activeElement.type === 'circle') setTabsItems(circleTabs);
+        else setTabsItems(regularTabs);
+    }, [isElementActive, activeElement]);
+
     return (
-        (
-            <div className="right" key={activeElementID}>
-                {(isElementActive && activeElement.type === 'circle') ?
-                    <>
-                        <Tabs defaultActiveKey={'style'} items={[{ key: 'circle', label: '循环依赖配置' }, { key: 'style', label: '样式', }]} size='large' type="card" centered onChange={(key) => { setActiveKey(key) }}/>
-                        {activeKey === 'style' && <StyleSelector></StyleSelector>}
-                        {activeKey === 'circle' && <CircleSelector></CircleSelector>}
-                    </>
-                    :
-                    <>
-                        <Tabs defaultActiveKey={activeKey} items={items} onChange={(key) => { setActiveKey(key) }} size='large' type="card" centered />
-                        {activeKey === 'style' && isElementActive && <StyleSelector></StyleSelector>}
-                        {activeKey === 'attr' && isElementActive && !activeElement.childrenElement && <AttrSelector></AttrSelector>}
-                        {activeKey === 'config' && <ConfigSelector></ConfigSelector>}
-                        {!isElementActive && activeKey !== 'config' && <Empty className='right-empty'></Empty>}
-                    </>}
-            </div>
-        )
+        <div className="right">
+            <Tabs
+                defaultActiveKey={activeKey}
+                items={tabsItems}
+                onChange={setActiveKey}
+                size='large'
+                type="card"
+                centered
+            />
+            {(isElementActive && activeElement.type === 'circle' && activeKey === 'circle') && <CircleSelector />}
+            {(isElementActive && activeKey === 'style') && <StyleSelector />}
+            {(isElementActive && activeKey === 'attr' && !activeElement.childrenElement) && <AttrSelector />}
+            {(activeKey === 'config') && <ConfigSelector />}
+            {!isElementActive && activeKey !== 'config' && <Empty className='right-empty'></Empty>}
+        </div>
     );
 }
 
